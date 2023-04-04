0978c473c804e44333b5df48e603e05223307c57
ashtonvcole <ashoncole1028@gmail.com>
2023-04-04 10:44:08 -0500

    Started main route

--- atx-traffic.py
+++ atx-traffic.py
@@ -55,9 +55,110 @@ def nil():
         None
 
     Returns:
-        An empty string.
+        An welcome string.
     """
-    return '';
+    return 'Welcome to atx-traffic!';
+
+
+
+
+
+# /incidents GET POST DELETE with optional query refinement parameters
+#     OR/AND toggle???
+#     id (string)
+#     issue (string)
+#     published (date range -- strings?)
+#     reported (date range -- strings?)
+#     latitude/longitude ranges (double)
+#     latitude/longitude center and radius (double)
+#     address contains (string)
+#     status (string)
+@app.route('/incidents', methods = ['GET', 'POST', 'DELETE'])
+def incidents():
+    """/incidents endpoint
+
+    This function either returns incident data, subject to certain query
+    parameters, updates the database with the latest source data, or clears
+    the database, depending on if the HTTP request method is GET, POST, or
+    DELETE, respectively.
+
+    Args:
+        None
+
+    Returns:
+        If the method is GET, a list of dictionaries representing each entry in
+            the database. If there is an error, a descriptive string will be
+            returned with a 404 status code. Note that sparse attributes are
+            excluded.
+        If the method is SET, a text message informing the user of success. If
+            there is an error, a descriptive string will be returned with a 404
+            status code.
+        If the method is DELETE, a text message informing the user of success.
+            If there is an error, a descriptive string will be returned with a
+            404 status code.
+    """
+    global rd, source_url
+    if request.method == 'GET':
+        try:
+            data = []
+            keys = rd.keys()
+            for key in keys:
+                # @TODO implement query parameters 
+                data.append(rd.hgetall(key))
+            return data
+        except Exception as e:
+            print(f'ERROR: unable to get data\n{e}')
+            return f'ERROR: unable to get data', 400
+    elif request.method == 'POST':
+        try:
+            the_json = requests.get(url = source_url).json()
+            data = the_json # @TODO check if any manipulation is needed here
+            for datum in data:
+                key = datum['traffic_report_id']
+                subkeys = datum.keys()
+                for subkey in subkeys:
+                    rd.hset(key, subkey, item[subkey])
+            return 'Data successfully posted', 200
+        except Exception as e:
+            print(f'ERROR: unable to post data\n{e}')
+            return f'ERROR: unable to post data', 400
+    elif request.method == 'DELETE':
+        try:
+            rd.flushdb()
+            return 'Data successfully deleted', 200
+        except Exception as e:
+            print(f'ERROR: unable to delete data\n{e}')
+            return f'ERROR: unable to delete data', 400
+
+
+
+
+
+# routes to help people form queries
+# /ids
+# /issues
+# /published-range
+# /reported-range
+# /coordinates-range
+# /addresses ... way they're recorded is irrecular
+# /statuses
+# /plot/dotmap
+#     issue (string)
+#     published (date range -- strings?)
+#     reported (date range -- strings?)
+#     latitude/longitude ranges (double)
+#     status (string)
+#     colorby (string...none, issue, or status)
+# /plot/heatmap
+#     issue (string)
+#     published (date range -- strings?)
+#     reported (date range -- strings?)
+#     latitude/longitude ranges (double)
+#     status (string)
+# /plot/timeseries
+# aaaaaaaaaaaaaaa
+# /help
+# Can write a text file that Python displays
 
 
 
