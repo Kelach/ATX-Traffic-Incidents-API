fa8f78772fb426a687a2e3ee77331c955328e98b
ashtonvcole <ashtoncole1028@gmail.com>
2023-04-20 02:54:26 +0000

    Added plots in src

--- scripts/worker.py
+++ scripts/worker.py
@@ -1,4 +1,6 @@
 from jobs import queue, rd, update_job_status, get_job_by_id
+from datetime import datetime
+import matplotlib.pyplot as plt
 
 # worker.py
 @queue.consume # decorator keeps function "live" and always reading new messages from the queue
@@ -14,7 +16,38 @@ def execute_job(jid):
         job_type = job.get("type")
         if job_type == "plot-timeseries":
             try:
-                pass # insert plot-timeseries function here
+                issue_reported = job.get('issue_reported"')
+                years = []
+                counts = []
+                for key in rd.keys():
+                    try:
+                        unix_time = int(rd.hget(key, 'published_date'))
+                        val = rd.hget(key, 'issue_reported"')
+                        year = datetime.utcfromtimestamp(unix_time).strftime('%Y')
+                        # Either val is the constrained issue_reported
+                        # Or issue_reported is left blank
+                        # In which case count everything
+                        if val == issue_reported \
+                                or issue_reported == "" \
+                                or issue_reported is None:
+                            # Either increase the count bin for a year
+                            # Or create a new year bin with a count of 1
+                            if year in years:
+                                ind = years.index(year)
+                                counts[ind] = counts[ind] + 1
+                            else:
+                                years.append(year)
+                                counts.append(1)
+                    except:
+                        continue
+                fig, ax = plt.subplots()
+                ax.bar(years, counts)
+                ax.title(f'{issue_reported} Cases over Time')
+                plt.savefig('plot.png') # temporarily saves in worker directory
+                with open('plot.png') as f:
+                    img = f.read()
+                update_job_status(jid, 'completed', img) # upload data to redis to return
+                #@TODO replace with web link?    
             except:
                 update_job_status(jid, "failed")
             pass
@@ -86,7 +119,7 @@ def execute_job(jid):
             print("Unable to read job_type")
             update_job_status(jid, "failed")
             return
-    ### update_job_status(jid, "completed", job)
+    ### update_job_status(jid, "completed", job) # THIS NESTS JOB WITHIN JOB
 
 def post_plot():
     # may need to implement time limits
