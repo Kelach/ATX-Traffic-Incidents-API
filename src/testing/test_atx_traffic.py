9c8efaf5f3b0fb275c79b7099175d8c2d6789700
Kelach <93161153+Kelach@users.noreply.github.com>
2023-04-25 10:05:24 -0500

    added more additional route testing

--- src/testing/test_atx_traffic.py
+++ src/testing/test_atx_traffic.py
@@ -2,6 +2,7 @@ import pytest
 import time
 import json
 from atx_traffic import get_seconds, is_in_bounds, app
+from jobs import add_job, delete_all_jobs, clear_queue
 
 def test_get_seconds():
     assert get_seconds("2037-12-30") == float(2145765600)
@@ -19,6 +20,16 @@ def test_is_in_bounds():
     assert is_in_bounds(check_address=False, incident=test_incident, radius_range=7, lng=lng, lat=lat) == True
     assert is_in_bounds(check_address=False, incident=test_incident, radius_range=5, lng=lng, lat=lat) == False
     
+def test_add_job():
+    assert len(add_job("12-12-2021", "12-12-2022", "incidents")) == 5
+def test_delete_all_jobs():
+    assert delete_all_jobs() == True
+
+def test_clear_queue():
+    assert clear_queue() == True
+
+
+
 def test_handle_jobs():
     payload = {"start": "wewewe", "end": ""}
     assert app.test_client().post("/jobs/incidents", data=json.dumps(payload)).status_code == 404
@@ -29,18 +40,32 @@ def test_handle_jobs():
     payload["start"] = "2019-12-30"
     payload["end"] = "2020-12-30"
     
-    assert app.test_client().post("/jobs/incidents", data=json.dumps(payload)).status_code == 200
-    assert app.test_client().post("/jobs/plot/timeseries", data=json.dumps(payload)).status_code == 200
-    assert app.test_client().post("/jobs/plot/dotmap", data=json.dumps(payload)).status_code == 200
-    assert app.test_client().post("/jobs/plot/heatmap", data=json.dumps(payload)).status_code == 200
+    # assert app.test_client().post("/jobs/incidents", data=json.dumps(payload)).status_code == 200
+    # assert app.test_client().post("/jobs/plot/timeseries", data=json.dumps(payload)).status_code == 200
+    # assert app.test_client().post("/jobs/plot/dotmap", data=json.dumps(payload)).status_code == 200
+    # assert app.test_client().post("/jobs/plot/heatmap", data=json.dumps(payload)).status_code == 200
 
-    assert app.test_client().get("/jobs/incidents/in-progress").data.decode("utf-8") == "incidents"
-    assert app.test_client().get("/jobs/plot/heatmap/in-progress").data.decode("utf-8") == "heatmap"
-    assert app.test_client().get("/jobs/plot/timeseries/in-progress").data.decode("utf-8") == "timeseries"
-    assert app.test_client().get("/jobs/plot/dotmap/in-progress").data.decode("utf-8") == "dotmap"
-    
+    # assert app.test_client().delete("/jobs", data=json.dumps(payload)).status_code == 200
 
+    job_incidents = json.loads(app.test_client().post("/jobs/incidents", data=json.dumps(payload)).data.decode("utf-8"))
+    job_timeseries = json.loads(app.test_client().post("/jobs/plot/timeseries", data=json.dumps(payload)).data.decode("utf-8"))
+    job_dotmap = json.loads(app.test_client().post("/jobs/plot/dotmap", data=json.dumps(payload)).data.decode("utf-8"))
+    job_heatmap = json.loads(app.test_client().post("/jobs/plot/heatmap", data=json.dumps(payload)).data.decode("utf-8"))
 
+    assert job_incidents["job_type"] == "incidents"
+    assert job_timeseries["job_type"] == "plot-timeseries"
+    assert job_dotmap["job_type"] == "plot-dotmap"
+    assert job_heatmap["job_type"] == "plot-heatmap"
+
+    assert len(json.loads(app.test_client().get("/jobs").data)) >= 4
+    assert len(json.loads(app.test_client().get("/jobs/incidents").data)) >= 1
+    assert len(json.loads(app.test_client().get("/jobs/plot/timeseries").data)) >= 1
+    assert len(json.loads(app.test_client().get("/jobs/plot/dotmap").data)) >= 1
+    assert len(json.loads(app.test_client().get("/jobs/plot/heatmap").data)) >= 1
+    assert len(json.loads(app.test_client().get("/jobs/plot").data)) >= 3
+
+    # assert app.test_client().delete("/jobs").status_code == 200
+    # assert len(json.loads(app.test_client().get("/jobs").data)) == 0 
 
 ##############################
 # Test Function Tingies here #
